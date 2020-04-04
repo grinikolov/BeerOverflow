229cf31cf9fb08bc0061a95b7fc1ddf5b7abab25
Ivan Lubomirov Ivanov <fizik_ivan@abv.bg>
2020-04-04 19:07:16 +0300

    Added a base for the project.

---
+++ BeerOverflow/BeerOverflowUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerOverflowUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
