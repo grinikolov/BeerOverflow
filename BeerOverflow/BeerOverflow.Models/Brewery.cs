7ea00ac7528284279edb4422f73c872c560bd6ba
Ivan Lubomirov Ivanov <fizik_ivan@abv.bg>
2020-04-11 07:14:57 +0300

    Brewery and Country Fix.

--- BeerOverflow/BeerOverflow.Models/Brewery.cs
+++ BeerOverflow/BeerOverflow.Models/Brewery.cs
@@ -1,25 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BeerOverflow.Models
 {
     public class Brewery
     {
-        public Guid ID { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
+        public int CountryID { get; set; }
         public Country Country { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-        [DataType(DataType.Date)]
-        public DateTime ModifiedOn { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime DeletedOn { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
         public bool IsDeleted { get; set; }
-
-
-
-
-
+        public ICollection<Beer> Beers { get; set; }
     }
 }
--- BeerOverflow/BeerOverflow.Models/Country.cs
+++ BeerOverflow/BeerOverflow.Models/Country.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace BeerOverflow.Models
 {
     public class Country
     {
-        public Guid ID { get; set; }
+        public Country()
+        {
+        }
+        public int ID { get; set; }
         public string Name { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-        [DataType(DataType.Date)]
-        public DateTime ModifiedOn { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime DeletedOn { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
         public bool IsDeleted { get; set; }
+        public ICollection<Brewery> Breweries { get; set; }
+
     }
 }
