da3c7e54ac33a60b8745aeb4434f3397b120669d
Ivan Lubomirov Ivanov <fizik_ivan@abv.bg>
2020-04-25 02:44:00 +0300

    Added View models for country and brewery as well as mappers to DTO to MVC controllers

---
+++ BeerOverflow/BeerOverflowAPI/Models/BreweryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerOverflowAPI.Models
+{
+    public class BreweryViewModel
+    {
+        public int? ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public ICollection<string> Beers { get; set; }
+    }
+}
